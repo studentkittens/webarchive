c15a73292d72e855f28af2534f843bd30e05cf9e
eddY <eschneider@hof-university.de>
2012-05-02 10:38:36 +0200

    Fixed some minor issues :D

--- src/webarchive/sv-client-prototyp/src/transfer/classes/Message.java
+++ src/webarchive/sv-client-prototyp/src/transfer/classes/Message.java
@@ -4,29 +4,24 @@ import headers.Header;
 import headers.classes.HandShakeHeader;
 import transfer.Transferable;
 
-public class HandShakeMessage implements Transferable {
+public class Message implements Transferable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 7455459151713375986L;
 
 	private final Header h;
 	private final Object data;
-	public HandShakeMessage()
+	public Message()
 	{
 		this.h = new HandShakeHeader();
 		this.data = null;
 	}
 	@Override
 	public Header getHeader() {
-		// TODO Auto-generated method stub
 		return h;
 	}
 
 	@Override
 	public Object getData() {
-		// TODO Auto-generated method stub
 		return data;
 	}
 
