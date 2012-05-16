427b1e955225c151038a47d91416d43870329a67
eddY <eschneider@hof-university.de>
2012-05-16 09:30:16 +0200

    changed createsql

--- src/sql/create.sql
+++ src/sql/create.sql
@@ -11,16 +11,16 @@ CREATE TABLE metaData (
 	metaId INTEGER PRIMARY KEY AUTOINCREMENT,
 	url TEXT UNIQUE NOT NULL,
 	path TEXT UNIQUE NOT NULL,
-	title TEXT,
 	mimeId INTEGER NOT NULL,
 	domainId INTEGER NOT NULL,
 	FOREIGN KEY (mimeId) REFERENCES mimeType(mimeId),
 	FOREIGN KEY (domainId) REFERENCES domain(domainId)
 );
-CREATE TABLE commitTag (
+CREATE TABLE history (
 	metaId INTEGER NOT NULL,
 	-- date time as "%yyyy-%MM-%ddT%hh:%mm:%ss"
 	commitTime TEXT NOT NULL,
+	title TEXT,
 	createTime TEXT NOT NULL,
 	PRIMARY KEY (commitTime), 
 	FOREIGN KEY (metaId) REFERENCES metaData(metaId)
