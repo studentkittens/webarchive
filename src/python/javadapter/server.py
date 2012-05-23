f7688b6e30ae9b2f01f34a1012e73c8670d54fbb
Chris P <sahib@online.de>
2012-05-23 23:13:34 +0200

    First working javadapter..

--- src/python/javadapter/server.py
+++ src/python/javadapter/server.py
@@ -1,59 +1,94 @@
 #!/usr/bin/env python
-# -*- encoding: latin1 -*-
+# encoding: utf-8
 
-# Dieser TCP-Server sendet alle Daten, die er empfängt, an den
-# Client als Antwort zurück
+"""
+###########################################################################
+#                               Javadapter                                #
+###########################################################################
 
-from socket import *
+A simple Server with a textprotocoll, representing the API to Java.
+
+TODO: Turn into a Object
+
+Usage: Start with python server.py <port>,
+       test with telnet localhost <port>
+"""
+
+import socket
 import sys
 
-# Der Standard-Echo-Port ist nicht sehr nützlich, da er root
-# Rechte erfordert. Wir nehmen den Port 50007
-ECHO_PORT = 50007
-BUFSIZE = 1024
+class UnknownCommand(Exception):
+    """
+    A sendable Exception :-P
+    """
+    def __init__(self, msg):
+        super(UnknownCommand, self).__init__(msg)
+        self.failure = b'ACK ' + msg 
+
+def build_error(*message):
+    """
+    Take an array of errors and turn it into
+    a valid error message suitable for sending back
+    """
+    return bytes('ACK ' + str(message), 'UTF-8')
+
+def handle_command(command, *args):
+    """
+    Take a command and it's arguments,
+    and try to see a meaning in them.
+
+    Returns: A 2 element tuple with a retcode string ('OK' / 'ACK ...'),
+             and response message that may be empty
+    """
+    print('Command received: ', command, '#', args)
+
+    response = b''
+
+    if command == b'checkout':
+        response = 'Doing a checkout, Sir.'
+    else:
+        raise UnknownCommand(b'Unknown command: ' + command)
+    return bytes(response, 'UTF-8')
+
+def send(conn, send_bytes):
+    """
+    Send a command to the clients
+    """
+    print('Sending:', send_bytes)
+    conn.sendall(send_bytes)
 
 def main():
-    # TCP-Socket erstellen und an Port binden
-    with socket(AF_INET, SOCK_STREAM) as s:
+    """
+    Needs to be turned into a Javadapter object
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind(('localhost', int(sys.argv[1])))
+        sock.listen(1)
 
-        try:
-            s.bind(('localhost', int(sys.argv[1])))
-            s.listen(1)
+        print("Server started on: {addr}".format(addr = sock.getsockname()))
 
-            print("Server started on: {addr}".format(addr = s.getsockname()))
+        # Warte auf eine Verbindung zum Server
+        conn, (remotehost, remoteport) = sock.accept()
+        print('Verbunden mit %s:%s' % (remotehost, remoteport))
 
-            # Warte auf eine Verbindung zum Server
-            conn, (remotehost, remoteport) = s.accept()
-            print('Verbunden mit %s:%s' % (remotehost, remoteport))
+        while True:
+            cmdlist = [x.strip() 
+                      for x in conn.recv(4096).split() 
+                      if len(x) > 0]
 
-            while True:
-                rc = 'OK\n'
-                response = ''
-                cmdlist = conn.recv(BUFSIZE).rstrip().split()
-                print('Got command:', cmdlist)
-                if cmdlist[0] == [b'checkout']:
-                    response = 'doing a ckout: %s' % cmdlist[1:]
-                elif cmdlist[0] == [b'commit']:
-                    response = 'doing a commit: %s' % cmdlist[1:]
-                else:
-                    rc = 'ACK unknown command\n'
-                     
+            if not cmdlist:
+                print('Got EOF')
+                break
 
-                
-                if not cmdlist:
-                    # Die Verbindung wurde getrennt
-                    # Abbruch
-                    print('Got EOF')
-                    break
-
-                if rc == 'OK':
-                    conn.send(bytes('\n'.join((response, rc)), 'UTF-8'))
-                else:
-                    conn.send(bytes(rc, 'UTF-8'))
-
-        except BaseException as e:
-            print('Error:', e)
-
+            try:
+                message = handle_command(cmdlist[0], *cmdlist[1:]) 
+            except UnknownCommand as exc:
+                send(conn, exc.failure + b'\n')
+            except Exception as exc:
+                print('Internal Error:', exc)
+                raise exc
+            else:
+                send(conn, message + b'\nOK\n')
 
 if __name__ == '__main__':
     main()
