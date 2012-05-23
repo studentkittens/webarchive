49c0ac09347bc3c3c3ce208f83f0cb72dce8626a
Chris P <sahib@online.de>
2012-05-23 21:09:45 +0200

    Merge Temp

--- src/python/javadapter/server.py
+++ src/python/javadapter/server.py
@@ -5,6 +5,7 @@
 # Client als Antwort zurück
 
 from socket import *
+import sys
 
 # Der Standard-Echo-Port ist nicht sehr nützlich, da er root
 # Rechte erfordert. Wir nehmen den Port 50007
@@ -14,34 +15,45 @@ BUFSIZE = 1024
 def main():
     # TCP-Socket erstellen und an Port binden
     with socket(AF_INET, SOCK_STREAM) as s:
-        s.bind(('localhost', ECHO_PORT))
-        s.listen(1)
 
-        print("Server started on: {addr}".format(addr = s.getsockname()))
+        try:
+            s.bind(('localhost', int(sys.argv[1])))
+            s.listen(1)
 
-        # Warte auf eine Verbindung zum Server
-        conn, (remotehost, remoteport) = s.accept()
-        print('Verbunden mit %s:%s' % (remotehost, remoteport))
+            print("Server started on: {addr}".format(addr = s.getsockname()))
 
-        while 1:
-            # Empfange daten vom Client
-            response = ''
-            cmdlist = conn.recv(BUFSIZE).rstrip().split()
-            if cmdlist == 'checkout':
-                response = 'doing a ckout: %s' % cmdlist[1:]
-            elif cmdlist == 'commit':
-                response = 'doing a commit: %s' % cmdlist[1:]
-            else:
-                response = 'unknown command'
-            
-            if not cmdlist:
-                # Die Verbindung wurde getrennt
-                # Abbruch
-                print('Got EOF')
-                break
+            # Warte auf eine Verbindung zum Server
+            conn, (remotehost, remoteport) = s.accept()
+            print('Verbunden mit %s:%s' % (remotehost, remoteport))
 
-            # Sende die Daten zurück zum client
-            conn.send(bytes(response,'UTF-8'))
+            while True:
+                rc = 'OK\n'
+                response = ''
+                cmdlist = conn.recv(BUFSIZE).rstrip().split()
+                print('Got command:', cmdlist)
+                if cmdlist[0] == [b'checkout']:
+                    response = 'doing a ckout: %s' % cmdlist[1:]
+                elif cmdlist[0] == [b'commit']:
+                    response = 'doing a commit: %s' % cmdlist[1:]
+                else:
+                    rc = 'ACK unknown command\n'
+                     
+
+                
+                if not cmdlist:
+                    # Die Verbindung wurde getrennt
+                    # Abbruch
+                    print('Got EOF')
+                    break
+
+                if rc == 'OK':
+                    conn.send(bytes('\n'.join((response, rc)), 'UTF-8'))
+                else:
+                    conn.send(bytes(rc, 'UTF-8'))
+
+        except BaseException as e:
+            print('Error:', e)
+
 
 if __name__ == '__main__':
     main()
