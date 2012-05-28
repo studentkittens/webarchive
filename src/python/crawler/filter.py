61081c6928360f6dcfc3db3528796fc9f2c4ee85
Chris P <sahib@online.de>
2012-05-28 19:43:59 +0200

    filter.py added

---
+++ src/python/crawler/filter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+__author__ = 'Christopher Pahl'
+
+
+def load(path = None):
+    if path is None:
+        actual_path = '/tmp/filter'
+    else:
+        actual_path = path
+
+def filter(meta_dict):
+    decision = True
+    return decision 
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
