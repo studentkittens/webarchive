f3e261d2bd1ebc2d9aef5c214c5ab1ebdadde0d5
Chris P <sahib@online.de>
2012-05-28 16:00:23 +0200

    added git wrapper

---
+++ src/python/crawler/git.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import subprocess
+import os
+
+__author__ = 'Christopher Pahl'
+
+class Git(object):
+    """A (overly-simple) Wrapper for the git binary"""
+    def __init__(self, domain):
+        """Instance a new Git Wrapper for a certain domain
+
+        :param domain: A domain found in the archive
+        """
+        # TODO: get the path to the domain
+        self.__domain = domain
+        self.__gitdir = os.path.join(self.__domain, '.git')
+        self.__basecmd = 'git --git-dir {git_dir} --work-tree {git_cwd} '.format(
+                git_dir = self.__gitdir,
+                git_cwd = self.__domain)
+
+    def __call(self, command):
+        """
+        Execute a git script
+
+        Prepend the basecmd to each line in command,
+        and subprocess.call() the resulting string synchronously
+
+        :param command: a multiline git script
+        :param forkShell: call subprocess.call with shell=True
+        :returns: the returncode of the last command in line
+        """
+        command = '\n'.join([self.__basecmd + line.strip()
+                            for line in command.splitlines()
+                            if len(line) > 0]) 
+
+        print('\n//////////////////\n')
+        print(command)
+        return subprocess.call(command, shell=True)
+
+    def init(self):
+        """
+        Create a new archive at specified domain path
+        
+        The target directory does not need to exit yet 
+
+        :returns: 0 on success, another rc on failure
+        """
+        return self.__call('init ' + self.__domain)
+
+    def checkout(self, target = 'master'):
+        """
+        checkout a certain point (tag, branch or commit)
+
+        :target: the target to visit
+        :returns: 0 on success, another rc on failure
+        """
+        return self.__call('checkout {}'.format(target))
+
+    def branch(self, branch_name = 'empty'):
+        """
+        create a new named branch
+
+        :branch_name: the name of the new branch, may not exist yet
+        :returns: 0 on success, another rc on failure
+        """
+        return self.__call('checkout -fb {}'.format(branch_name))
+
+    def commit(self, message = 'edit'):
+        """
+        commit any changes made 
+
+        git add . and git commit -am <message> is done
+
+        :message: The commit message
+        :returns: 0 on success, another rc on failure
+        """
+        return self.__call("""
+                   add {path} &&
+                   commit -am '{msg}'""".format(
+                       path = self.__domain,
+                       msg  = message))
+
+
+if __name__ == '__main__':
+    import unittest
+
+    TEST_DIR = '/tmp/git_test/'
+    class GitTest(unittest.TestCase):
+        def setUp(self):
+            self.__repo = Git(TEST_DIR)
+            self.__repo.init()
+
+        def add_file(self, name, mode):
+            with open(os.path.join(TEST_DIR, name), mode) as dummy:
+                dummy.write('Hello Kitteh!')
+
+        def test_commit(self):
+            self.add_file('file', 'w')
+            self.assertEqual(self.__repo.commit(message = 'init'), 0)
+
+        def test_branch(self):
+            # make empty master
+            self.add_file('empty', 'w')
+            self.assertEqual(self.__repo.commit(), 0)
+
+            # branch to a new branch and commit something
+            self.assertEqual(self.__repo.branch('2405TT2323'), 0)
+            self.add_file('file_new', 'w')
+            self.assertEqual(self.__repo.commit(), 0)
+
+            # change back to master
+            self.assertEqual(self.__repo.checkout(), 0)
+            self.add_file('file_new', 'w')
+            self.assertEqual(self.__repo.commit(), 0)
+            
+        def tearDown(self):
+            subprocess.call(['rm', '-rf', TEST_DIR])
+            pass
+            
+    unittest.main()
