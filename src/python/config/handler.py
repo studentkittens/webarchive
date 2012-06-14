f32f8171eb1ef084454930f11ff37dfe81ab2750
unknown <Kenny@Kenny-PC.(none)>
2012-06-14 16:33:33 +0200

    Config Handler

---
+++ src/python/config/handler.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import reader
+import writer
+
+def get(value):
+    return reader.get(value)
+
+def get_default(value):
+    return reader.get_default(value)
+
+def get_with_default(value):
+    return reader.get_with_default(value)
+
+def set(value, str):
+    return writer.set(value, str)
+   
+def set_default(value):
+    return writer.set_default(value)
---
+++ src/python/config/writer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+import options
+import reader
+
+def set(value, st):
+    if (value == ('crawler.depth' or 'crawler.maxInst' or 'server.port' or 
+        'server.notify.interval' or 'javadapter.port')):
+        try:
+            var1 = int(st)
+            options.actual_options[value]= var1
+            return value + ' is set to ' + str(var1)
+        except ValueError:
+            return 'NAN'
+        
+    if (value == 'crawler.interval'):
+        try:
+            var2 = float(st)
+            options.actual_options[value]= var2
+            return value + ' is set to ' + str(var2)
+        except ValueError:
+            return 'NAN'
+        
+    else:
+        try:
+            options.actual_options[value] = st
+            return value + ' is set to ' + st
+        except KeyError:
+            return value + ' is not a Item'
+
+def set_default(value):
+    try:
+        options.actual_options[value] = reader.get_default(value)
+        return value + ' set to ' + options.actual_options[value]
+    except KeyError:
+        return value + ' is not a Item'
